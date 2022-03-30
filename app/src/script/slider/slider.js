c77d848574351202d32946d0e09391f02b5c2235
Timdit <timur0809tim@gmail.com>
2022-03-30 11:07:42 +0500

    tryCatch

--- app/src/script/slider/slider.js
+++ app/src/script/slider/slider.js
@@ -1,103 +1,105 @@
-class Slider {
-    constructor(obj) {
-        this.slider = document.querySelector(obj.slider)
-        this.sliderLine = this.slider.querySelector(obj.sliderLine)
-        this.slides = [...this.sliderLine.children]
-        this.next = this.slider.querySelector(obj.next)
-        this.prev = this.slider.querySelector(obj.prev)
+try {
+    class Slider {
+        constructor(obj) {
+            this.slider = document.querySelector(obj.slider)
+            this.sliderLine = this.slider.querySelector(obj.sliderLine)
+            this.slides = [...this.sliderLine.children]
+            this.next = this.slider.querySelector(obj.next)
+            this.prev = this.slider.querySelector(obj.prev)
 
-        this.width = this.slider.clientWidth
-        this.height = this.slider.clientHeight
-        this.dir = obj.direction.toUpperCase() == 'X' ? 'X' : 'Y'
-        this.moveSize = this.dir === 'X' ? this.width : this.height
+            this.width = this.slider.clientWidth
+            this.height = this.slider.clientHeight
+            this.dir = obj.direction.toUpperCase() == 'X' ? 'X' : 'Y'
+            this.moveSize = this.dir === 'X' ? this.width : this.height
 
-        this.activeSlide = 0
+            this.activeSlide = 0
 
-        this.sliderLine.style = `
-            position: relative;
-            height: ${this.height}px;
-            overflow: hidden;
-        `
+            this.sliderLine.style = `
+                position: relative;
+                height: ${this.height}px;
+                overflow: hidden;
+            `
 
-        this.slides.forEach((slide, i) => {
-            slide.style = `
-            position: absolute;
-            height: ${this.height}px;
-            width: ${this.width}px;
-           `
-            if (i != this.activeSlide) {
-                slide.style.transform = `translate${this.dir}(${this.moveSize}px)`
-            }
+            this.slides.forEach((slide, i) => {
+                slide.style = `
+                position: absolute;
+                height: ${this.height}px;
+                width: ${this.width}px;
+               `
+                if (i != this.activeSlide) {
+                    slide.style.transform = `translate${this.dir}(${this.moveSize}px)`
+                }
 
-            if (i === this.slides.length - 1) {
-                slide.style.transform = `translate${this.dir}(${-this.moveSize}px)`
-            }
-        })
-        
-        let interval = setInterval(() => {
-            this.move(this.next)
-        }, 5000)
-        
-        this.slider.addEventListener('mouseover', () => {
-            clearInterval(interval)
-        })
-        
-        this.slider.addEventListener('mouseleave', () => {
-            interval = setInterval(() => {
+                if (i === this.slides.length - 1) {
+                    slide.style.transform = `translate${this.dir}(${-this.moveSize}px)`
+                }
+            })
+
+            let interval = setInterval(() => {
                 this.move(this.next)
             }, 5000)
-        })
-        
 
-        this.prev.addEventListener('click', () => this.move(this.prev))
-        this.next.addEventListener('click', () => this.move(this.next))
+            this.slider.addEventListener('mouseover', () => {
+                clearInterval(interval)
+            })
 
-    }
+            this.slider.addEventListener('mouseleave', () => {
+                interval = setInterval(() => {
+                    this.move(this.next)
+                }, 5000)
+            })
 
-    move(btn) {
-        let btnLeftOrRight = btn == this.next ? this.moveSize * -1 : this.moveSize
 
-        this.slides.forEach((slide, i) => {
-            slide.style.transition = '0s'
-            if (i != this.activeSlide) {
-                slide.style.transform = `translate${this.dir}(${btnLeftOrRight * -1}px)`
-            }
-        })
+            this.prev.addEventListener('click', () => this.move(this.prev))
+            this.next.addEventListener('click', () => this.move(this.next))
 
-        this.slides[this.activeSlide].style.transform = `translate${this.dir}(${btnLeftOrRight}px)`
-        this.slides[this.activeSlide].style.transition = '1s'
-
-        if (btn == this.next) {
-            this.activeSlide++
-            if (this.activeSlide >= this.slides.length) {
-                this.activeSlide = 0
-            }
-        } else if (btn == this.prev) {
-            this.activeSlide--
-            if (this.activeSlide <= 0) {
-                this.activeSlide = this.slides.length - 1
-            }
         }
 
-        this.slides[this.activeSlide].style.transform = `translate${this.dir}(0px)`
-        this.slides[this.activeSlide].style.transition = '1s'
+        move(btn) {
+            let btnLeftOrRight = btn == this.next ? this.moveSize * -1 : this.moveSize
 
-        this.prev.setAttribute('disabled', 'disabled')
-        this.next.setAttribute('disabled', 'disabled')
-        
-        setTimeout(() => {
-            this.prev.removeAttribute('disabled')
-            this.next.removeAttribute('disabled')
-        }, 1500);
+            this.slides.forEach((slide, i) => {
+                slide.style.transition = '0s'
+                if (i != this.activeSlide) {
+                    slide.style.transform = `translate${this.dir}(${btnLeftOrRight * -1}px)`
+                }
+            })
+
+            this.slides[this.activeSlide].style.transform = `translate${this.dir}(${btnLeftOrRight}px)`
+            this.slides[this.activeSlide].style.transition = '1s'
+
+            if (btn == this.next) {
+                this.activeSlide++
+                if (this.activeSlide >= this.slides.length) {
+                    this.activeSlide = 0
+                }
+            } else if (btn == this.prev) {
+                this.activeSlide--
+                if (this.activeSlide <= 0) {
+                    this.activeSlide = this.slides.length - 1
+                }
+            }
+
+            this.slides[this.activeSlide].style.transform = `translate${this.dir}(0px)`
+            this.slides[this.activeSlide].style.transition = '1s'
+
+            this.prev.setAttribute('disabled', 'disabled')
+            this.next.setAttribute('disabled', 'disabled')
+
+            setTimeout(() => {
+                this.prev.removeAttribute('disabled')
+                this.next.removeAttribute('disabled')
+            }, 1500);
+        }
     }
-}
 
-const slider = new Slider({
-    slider: '.main__content-slider',
-    sliderLine: '.main__content-sliderLine',
-    next: '.main__content-sliderNext',
-    prev: '.main__content-sliderPrev',
-    direction: 'X',
-})
+    const slider = new Slider({
+        slider: '.main__content-slider',
+        sliderLine: '.main__content-sliderLine',
+        next: '.main__content-sliderNext',
+        prev: '.main__content-sliderPrev',
+        direction: 'X',
+    })
 
+} catch (e) {}
 export default Slider
